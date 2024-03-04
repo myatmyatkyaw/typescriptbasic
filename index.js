d96bfd09448825b75c2cb19b8d1a62361b6fd399
myatmyatkyaw <myatmyatkyaw@gicjp.com>
2024-03-04 14:37:08 +0630

    lesson2

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
 "use strict";
-let person = 'myat';
-let age = 3;
-let single = true;
-let ages = [1, 2, 3, 4];
-ages.push(5);
-console.log(ages);
-let people = ['a', 'b', 'c'];
-people.push('d');
-console.log(people);
-let me = {
-    name: 'sakura',
-    age: 20,
-    single: false
-};
-me.age = 'su';
-console.log(me);
+// let person:string = 'myat';
+// let age:number = 3;
+// let single:boolean = true;
+// let ages = [1,2,3,4];
+// ages.push(5);
+// console.log(ages);
+// let people = ['a','b','c'];
+// people.push('d');
+// console.log(people);
+// let me = {
+//     name: 'sakura',
+//     age: 20,
+//     single: false
+// }
+// me.age = 9;
+// console.log(me);
+//for function
+function addNumber(a, b) {
+    return a + b;
+}
+let a = addNumber(4, 5);
+console.log(a);
+function addPeople(a, b) {
+    return a + b;
+}
+let c = addPeople('myat ', 'kyaw');
+console.log(c);
+function addString(d, e) {
+    return d + e;
+}
+//let f = addString(3 , 'luffy');
+console.log(addString(3, 'luffy'));
--- index.ts
+++ index.ts
@@ -1,17 +1,35 @@
-let person:string = 'myat';
-let age:number = 3;
-let single:boolean = true;
-let ages = [1,2,3,4];
-ages.push(5);
-console.log(ages);
-let people = ['a','b','c'];
-people.push('d');
-console.log(people);
+// let person:string = 'myat';
+// let age:number = 3;
+// let single:boolean = true;
+// let ages = [1,2,3,4];
+// ages.push(5);
+// console.log(ages);
+// let people = ['a','b','c'];
+// people.push('d');
+// console.log(people);
 
-let me = {
-    name: 'sakura',
-    age: 20,
-    single: false
+// let me = {
+//     name: 'sakura',
+//     age: 20,
+//     single: false
+// }
+// me.age = 9;
+// console.log(me);
+
+//for function
+function addNumber(a: number ,b: number){
+    return a + b;
 }
-me.age = 'su';
-console.log(me);
+let a = addNumber(4,5);
+console.log(a)
+
+function addPeople(a:string , b:string){
+    return a + b;
+}
+let c = addPeople('myat ','kyaw');
+console.log(c)
+
+function addString(d:number, e:string){
+    return d + e;
+}
+console.log(addString(3, 'luffy'));
