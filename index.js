fef9e2810ff2d2763a2afc65c279dadff9abd537
myatmyatkyaw <myatmyatkyaw@gicjp.com>
2024-03-04 13:42:18 +0630

    lesson1

---
+++ index.js
@@ -0,0 +1,17 @@
+"use strict";
+let person = 'myat';
+let age = 3;
+let single = true;
+let ages = [1, 2, 3, 4];
+ages.push(5);
+console.log(ages);
+let people = ['a', 'b', 'c'];
+people.push('d');
+console.log(people);
+let me = {
+    name: 'sakura',
+    age: 20,
+    single: false
+};
+me.age = 'su';
+console.log(me);
---
+++ index.ts
@@ -0,0 +1,17 @@
+let person:string = 'myat';
+let age:number = 3;
+let single:boolean = true;
+let ages = [1,2,3,4];
+ages.push(5);
+console.log(ages);
+let people = ['a','b','c'];
+people.push('d');
+console.log(people);
+
+let me = {
+    name: 'sakura',
+    age: 20,
+    single: false
+}
+me.age = 'su';
+console.log(me);
