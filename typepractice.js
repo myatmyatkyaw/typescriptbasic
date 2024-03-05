d4e1883a9847da8c3c881a2ccfccea76d3ad348d
myatmyatkyaw <myatmyatkyaw@gicjp.com>
2024-03-05 08:55:02 +0630

    type

---
+++ typepractice.js
@@ -0,0 +1,9 @@
+"use strict";
+function testPractice() {
+    const a = 'myat';
+    const b = 'kyaw';
+    const c = 24;
+    return [a, b, c];
+}
+const haha = testPractice();
+console.log(haha);
---
+++ typepractice.ts
@@ -0,0 +1,10 @@
+type rgb = [string, string, number];
+
+function testPractice() : rgb {
+    const a = 'myat'
+    const b = 'kyaw'
+    const c = 24
+    return [a,b,c]
+}
+const haha = testPractice()
+console.log(haha)
