eb59cc99297df2de53c894d35129e3559ca424d2
myatmyatkyaw <myatmyatkyaw@gicjp.com>
2024-03-05 09:59:45 +0630

    type guard

--- typepractice.js
+++ typepractice.js
@@ -15,3 +15,24 @@ function createUser(user) {
     console.log(`Name is ${user.name} and age is ${user.age}`);
 }
 createUser(userone);
+createUser({ name: 'yoshi', age: 34 });
+const useroneone = {
+    type: 'user',
+    username: 'jennie',
+    age: 26
+};
+const usertwo = {
+    type: 'person',
+    name: 'jisoo',
+    age: 30
+};
+function userDetails(value) {
+    if (value.type === 'user') {
+        console.log(`the user is ${value.username} and ${value.age}`);
+    }
+    else {
+        console.log(`the person is ${value.name} and ${value.age}`);
+    }
+}
+userDetails(usertwo);
+userDetails(useroneone);
--- typepractice.ts
+++ typepractice.ts
@@ -21,3 +21,38 @@ function createUser(user: User): void {
     console.log(`Name is ${user.name} and age is ${user.age}`)
 }
 createUser(userone)
+createUser({name: 'yoshi', age: 34})
+
+
+interface Userone {
+    type: 'user',
+    username: string,
+    age: number,
+}
+interface Person{
+    type: 'person',
+    name: string,
+    age: number
+}
+const useroneone : Userone = {
+    type : 'user',
+    username : 'jennie',
+    age : 26
+}
+const usertwo : Person = {
+    type: 'person',
+    name: 'jisoo',
+    age: 30
+}
+
+function userDetails(value : Userone | Person) : void {
+    if(value.type=== 'user'){
+        console.log(`the user is ${value.username} and ${value.age}`)
+    }
+    else{
+        console.log(`the person is ${value.name} and ${value.age}`)
+    }
+}
+userDetails(usertwo)
+userDetails(useroneone)
+
